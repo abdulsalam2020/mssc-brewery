a0afe9c69587c91711f826cc782bcb29fd0e7d85
Abdul Salam <abdulsalam@live.in>
2020-09-28 11:55:22 +0530

    Added Customer Dto service and controller

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbrewery.services;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public CustomerDto getCustomerById(UUID id) {
+		return CustomerDto.builder()
+				.id(id)
+				.name("Tom")
+				.build();
+	}
+
+}
