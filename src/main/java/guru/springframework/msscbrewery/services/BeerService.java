4f6951cd08a996e42daa4e11763f5a8264ddb94b
Abdul Salam <abdulsalam@live.in>
2020-09-28 13:40:24 +0530

    added post, put and delete mapping for BeerController

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -8,5 +8,9 @@ import java.util.UUID;
  * Created by jt on 2019-04-20.
  */
 public interface BeerService {
-    BeerDto getBeerById(UUID beerId);
+    public BeerDto getBeerById(UUID beerId);
+    public BeerDto saveNewBeer(BeerDto beerDto);
+	public void updateBeer(UUID beerId, BeerDto beerDto);
+	public void deleteById(UUID beerId);
+    
 }
